25976c41de32c11646c3d5cbbce2fe7bfab483e6
Evan Luo <zl0402@fastmail.com>
2024-09-25 16:57:00 +0800

    feat: Implement logger for TypeORM using Winston

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,39 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm"
+import {DataSource, Logger, QueryRunner} from "typeorm"
+import {logger} from "./utils/logger";
+
+export class WinstonTypeormLogger implements Logger {
+  log(level: "log" | "info" | "warn", message: any, _queryRunner?: QueryRunner): any {
+    switch (level) {
+      case "info":
+      case "log":
+        logger.info(message)
+        break
+      case "warn":
+        logger.warn(message)
+    }
+  }
+
+  logMigration(message: string, _queryRunner?: QueryRunner): any {
+    logger.info(message)
+  }
+
+  logQuery(query: string, _parameters?: any[], _queryRunner?: QueryRunner): any {
+    logger.debug(query)
+  }
+
+  logQueryError(error: string | Error, query: string, _parameters?: any[], _queryRunner?: QueryRunner): any {
+    logger.error(`Error: ${error} when performing query: ${query}`)
+  }
+
+  logQuerySlow(_time: number, query: string, _parameters?: any[], _queryRunner?: QueryRunner): any {
+    logger.debug(query)
+  }
+
+  logSchemaBuild(message: string, _queryRunner?: QueryRunner): any {
+    logger.debug(message)
+  }
+}
 
 export const dataSource = new DataSource({
   type: "postgres",
@@ -9,7 +43,8 @@ export const dataSource = new DataSource({
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
   synchronize: true,
-  logging: false,
+  logging: true,
+  logger: new WinstonTypeormLogger(),
   entities: ["src/modules/**/*.entity.ts"],
   migrations: [],
   subscribers: [],
